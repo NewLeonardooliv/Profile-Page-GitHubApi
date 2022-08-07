54df9b00ec56a61aca0aa7919e2d258bf96b7ca5
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2022-08-07 15:38:36 -0300

    profile and repositories page

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { api } from "../service/api";
+
+type Profile = {
+    login: string,
+    id: number,
+    avatar_url: string
+    gravatar_id: string,
+    html_url: string,
+    repos_url: string,
+    received_events_url: string,
+    type: string,
+    name: string,
+    company: string,
+    location: string,
+    hireable: string,
+    bio: string,
+}
+
 export function HomePage() {
+    const [user, setProfile] = useState<Profile[]>([]);
+    useEffect(() => {
+        document.title = 'Home · ProfilePage';
+
+        api.get("/users/NewLeonardooliv").then(response => {
+            setProfile(response.data);
+        })
+    }, []);
+
     return (
-        <h1>Home</h1>
+        <div>
+            {user.map(prof => {
+                return (
+                    <div key={prof.id}>{prof.name}</div>
+                )
+            })}
+        </div>
     );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./HomePage/HomePage";
+import { RepositoriesPage } from "./Resitories/RepositoriesPage";
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/repositories" element={<RepositoriesPage />} />
             </Routes>
         </Router>
     );
